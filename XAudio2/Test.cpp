993a1e04c720480b39b4e0e14568ce25d7552b15
lizyoko9 <879039681@qq.com>
2022-07-06 13:35:21 +0800

    Initialize

---
+++ XAudio2/Test.cpp
@@ -0,0 +1,185 @@
+﻿#include <iostream>
+//必要的头文件
+#include <Windows.h>
+#include <xaudio2.h>
+#ifdef _XBOX //Big-Endian
+#define fourccRIFF 'RIFF'
+#define fourccDATA 'data'
+#define fourccFMT 'fmt '
+#define fourccWAVE 'WAVE'
+#define fourccXWMA 'XWMA'
+#define fourccDPDS 'dpds'
+#endif
+
+#ifndef _XBOX //Little-Endian
+#define fourccRIFF 'FFIR'
+#define fourccDATA 'atad'
+#define fourccFMT ' tmf'
+#define fourccWAVE 'EVAW'
+#define fourccXWMA 'AMWX'
+#define fourccDPDS 'sdpd'
+#endif
+
+//声明波形图、Buffer、音频引擎等
+HRESULT hr;
+IXAudio2* pXAudio2 = nullptr;
+IXAudio2MasteringVoice* pMasterVoice = nullptr;
+WAVEFORMATEXTENSIBLE wfx = { 0 };
+XAUDIO2_BUFFER buffer = { 0 };
+IXAudio2* XAudioEngine = NULL;
+
+//初始化函数
+BOOL Init()
+{
+	hr = CoInitializeEx(nullptr, COINIT_MULTITHREADED);
+	if (FAILED(hr))
+		return hr;
+
+	if (FAILED(hr = XAudio2Create(&pXAudio2, 0, XAUDIO2_DEFAULT_PROCESSOR)))
+		return hr;
+
+	if (FAILED(hr = pXAudio2->CreateMasteringVoice(&pMasterVoice)))
+		return hr;
+
+	return true;
+}
+
+//释放
+void Uninit()
+{
+    if(XAudioEngine)
+        XAudioEngine->Release();
+    CoUninitialize();
+}
+
+//查找Riff区块
+HRESULT FindChunk(HANDLE hFile, DWORD fourcc, DWORD& dwChunkSize, DWORD& dwChunkDataPosition)
+{
+    HRESULT hr = S_OK;
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
+        return HRESULT_FROM_WIN32(GetLastError());
+
+    DWORD dwChunkType;
+    DWORD dwChunkDataSize;
+    DWORD dwRIFFDataSize = 0;
+    DWORD dwFileType;
+    DWORD bytesRead = 0;
+    DWORD dwOffset = 0;
+
+    while (hr == S_OK)
+    {
+        DWORD dwRead;
+        if (0 == ReadFile(hFile, &dwChunkType, sizeof(DWORD), &dwRead, NULL))
+            hr = HRESULT_FROM_WIN32(GetLastError());
+
+        if (0 == ReadFile(hFile, &dwChunkDataSize, sizeof(DWORD), &dwRead, NULL))
+            hr = HRESULT_FROM_WIN32(GetLastError());
+
+        switch (dwChunkType)
+        {
+        case fourccRIFF:
+            dwRIFFDataSize = dwChunkDataSize;
+            dwChunkDataSize = 4;
+            if (0 == ReadFile(hFile, &dwFileType, sizeof(DWORD), &dwRead, NULL))
+                hr = HRESULT_FROM_WIN32(GetLastError());
+            break;
+
+        default:
+            if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, dwChunkDataSize, NULL, FILE_CURRENT))
+                return HRESULT_FROM_WIN32(GetLastError());
+        }
+
+        dwOffset += sizeof(DWORD) * 2;
+
+        if (dwChunkType == fourcc)
+        {
+            dwChunkSize = dwChunkDataSize;
+            dwChunkDataPosition = dwOffset;
+            return S_OK;
+        }
+
+        dwOffset += dwChunkDataSize;
+
+        if (bytesRead >= dwRIFFDataSize) return S_FALSE;
+
+    }
+
+    return S_OK;
+
+}
+
+//从区块中读取数据
+HRESULT ReadChunkData(HANDLE hFile, void* buffer, DWORD buffersize, DWORD bufferoffset)
+{
+    HRESULT hr = S_OK;
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, bufferoffset, NULL, FILE_BEGIN))
+        return HRESULT_FROM_WIN32(GetLastError());
+    DWORD dwRead;
+    if (0 == ReadFile(hFile, buffer, buffersize, &dwRead, NULL))
+        hr = HRESULT_FROM_WIN32(GetLastError());
+    return hr;
+}
+
+int main()
+{
+	Init();
+
+    //打开音频文件
+#ifdef _XBOX
+    char* strFileName = "game:\\media\\MusicMono.wav";
+#else
+    const TCHAR* strFileName = TEXT("C:\\Users\\wangb\\Desktop\\Music\\DEMO.wav");
+#endif
+    HANDLE hFile = CreateFile(
+        strFileName,
+        GENERIC_READ,
+        FILE_SHARE_READ,
+        NULL,
+        OPEN_EXISTING,
+        0,
+        NULL);
+
+    if (INVALID_HANDLE_VALUE == hFile)
+    {
+	    std::cout << "INVALID_HANDLE_VALUE";
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+
+    if (INVALID_SET_FILE_POINTER == SetFilePointer(hFile, 0, NULL, FILE_BEGIN))
+    {
+        std::cout << "INVALID_SET_FILE_POINTER";
+        return HRESULT_FROM_WIN32(GetLastError());
+    }
+    //寻找Riff区块，检查文件类型
+    DWORD dwChunkSize;
+    DWORD dwChunkPosition;
+
+    FindChunk(hFile, fourccRIFF, dwChunkSize, dwChunkPosition);
+    DWORD filetype;
+    ReadChunkData(hFile, &filetype, sizeof(DWORD), dwChunkPosition);
+    if (filetype != fourccWAVE){return S_FALSE;}
+
+    //找到fmt区块，将内容复制到XTENSIBLE中
+    FindChunk(hFile, fourccFMT, dwChunkSize, dwChunkPosition);
+    ReadChunkData(hFile, &wfx, dwChunkSize, dwChunkPosition);
+
+    //找到data区块，将内容读入缓冲区
+    FindChunk(hFile, fourccDATA, dwChunkSize, dwChunkPosition);
+    BYTE* pDataBuffer = new BYTE[dwChunkSize];
+    ReadChunkData(hFile, pDataBuffer, dwChunkSize, dwChunkPosition);
+
+    //填充Buffer结构
+    buffer.AudioBytes = dwChunkSize;  //audio buffer的大小，byte大小
+    buffer.pAudioData = pDataBuffer;  //包含音频数据的Buffer
+    buffer.Flags = XAUDIO2_END_OF_STREAM; //音频流结束标志
+
+    IXAudio2SourceVoice* pSourceVoice;
+    if (FAILED(hr = pXAudio2->CreateSourceVoice(&pSourceVoice, (WAVEFORMATEX*)&wfx))) 
+        return hr;
+    if (FAILED(hr = pSourceVoice->SubmitSourceBuffer(&buffer)))
+        return hr;
+    if (FAILED(hr = pSourceVoice->Start(0)))
+        return hr;
+
+    Uninit();
+}
